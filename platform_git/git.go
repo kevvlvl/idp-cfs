7c267d32d8b29d779cd7018676b7319fad9ccc23
Kev <ksayad@gmail.com>
2023-12-05 22:41:11 -0500

    pushfiles

--- platform_git/git.go
+++ platform_git/git.go
@@ -3,6 +3,8 @@ package platform_git
 import (
 	"errors"
 	"github.com/rs/zerolog/log"
+	"os"
+	"path/filepath"
 )
 
 func GetCode(tool string) *GitCode {
@@ -56,5 +58,21 @@ func (c *GitCode) CreateRepository(name string) error {
 }
 
 func (c *GitCode) PushFiles(r *Repository, gpPath string) error {
+
+	var files []string
+
+	err := filepath.Walk(gpPath, func(filepath string, info os.FileInfo, err error) error {
+
+		files = append(files, filepath)
+		return nil
+	})
+	if err != nil {
+		log.Error().Msgf("Failed to walk the directory %s. Error: %v", gpPath, err)
+	}
+
+	log.Info().Msgf("List of files to commit: %+v", files)
+
+	// TODO use go-git to connect to the repository (r) and add the files (files) add, commit, and push
+
 	return errors.New("not implemented yet")
 }
