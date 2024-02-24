3da2bce319ce9c58e90d647ae09c1959e29aab09
Kev <ksayad@gmail.com>
2024-02-23 23:29:19 -0500

    reduce toUpper multiple calls

--- git_api/auth.go
+++ git_api/auth.go
@@ -9,9 +9,10 @@ import (
 func getAuth(tool string) *GitApiAuth {
 
 	auth := &GitApiAuth{}
+	toolUpper := strings.ToUpper(tool)
 
-	user := os.Getenv(fmt.Sprintf("CFS_CODE_%s_USER", strings.ToUpper(tool)))
-	token := os.Getenv(fmt.Sprintf("CFS_CODE_%s_PAT", strings.ToUpper(tool)))
+	user := os.Getenv(fmt.Sprintf("CFS_CODE_%s_USER", toolUpper))
+	token := os.Getenv(fmt.Sprintf("CFS_CODE_%s_PAT", toolUpper))
 
 	if user != "" && token != "" {
 		auth.codeUser = user
@@ -19,8 +20,8 @@ func getAuth(tool string) *GitApiAuth {
 		auth.codeDefined = true
 	}
 
-	user = os.Getenv(fmt.Sprintf("CFS_GP_%s_USER", strings.ToUpper(tool)))
-	token = os.Getenv(fmt.Sprintf("CFS_GP_%s_PAT", strings.ToUpper(tool)))
+	user = os.Getenv(fmt.Sprintf("CFS_GP_%s_USER", toolUpper))
+	token = os.Getenv(fmt.Sprintf("CFS_GP_%s_PAT", toolUpper))
 
 	if user != "" && token != "" {
 		auth.gpUser = user
